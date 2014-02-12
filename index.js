579c6ac00f81234b366e37f2549e367486363755
Michael Williams <dinosaur@riseup.net>
2014-02-12 15:12:30 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+module.exports.Model = {
+  componentDidMount: function() {
+    // Whenever there may be a change in the Backbone data, trigger a reconcile.
+    this.getBackboneModels().forEach(this.injectModel, this);
+  },
+  componentWillUnmount: function() {
+    // Ensure that we clean up any dangling references when the component is
+    // destroyed.
+    this.__syncedModels.forEach(function(model) {
+      model.off(null, model.__updater, this);
+    }, this);
+  },
+  injectModel: function(model){
+    if(!this.__syncedModels) this.__syncedModels = [];
+    if(!~this.__syncedModels.indexOf(model)){
+      var updater = this.forceUpdate.bind(this, null);
+      model.__updater = updater;
+      model.on('add change remove', updater, this);
+      this.__syncedModels.push(model);
+    }
+  }
+}
+
+module.exports.Bind = {
+  bindTo: function(model, key){
+    return {
+      value: model.get(key),
+      requestChange: function(value){
+          model.set(key, value);
+      }.bind(this)
+    }
+  }
+}
